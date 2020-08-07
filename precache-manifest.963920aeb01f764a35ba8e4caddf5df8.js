1c8a8e63d6280df1eb8486ab22083275e75e29d7
gaurav24bisht <51336013+gaurav24bisht@users.noreply.github.com>
2020-08-07 19:55:42 +0530

    Updates

---
+++ precache-manifest.963920aeb01f764a35ba8e4caddf5df8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1571760ba63a6df3a402bc7c6eb308b7",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "f7f41e29258d2a3af9e2",
+    "url": "/monster/static/css/main.df6eef79.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/monster/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7f41e29258d2a3af9e2",
+    "url": "/monster/static/js/main.2919f719.chunk.js"
+  },
+  {
+    "revision": "ce4106392ac25c68b11d",
+    "url": "/monster/static/js/runtime-main.9dda15a6.js"
+  }
+]);
